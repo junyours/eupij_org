a7b003b27e2f0e1ef333a711eabe4a5f6ccb58b6
ac-devs19 <ac.devs.07192002@gmail.com>
2026-04-06 14:19:21 +0800

    first commit

---
+++ resources/views/pages/web/category/about-publisher.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.web')
+
+@section('content')
+  <div class="font-medium text-justify space-y-4">
+    <h1 class="font-bold text-base border-b border-slate-300 pb-4">About the Publisher</h1>
+    <div class="space-y-2">
+      <p>The Environmental and Urban Planning International Journal (EUPIJ) reflects the institution’s commitment of
+        PHILIPPINE INSTITUTE OF ENVIRONMENTAL PLANNERS (PIEP) to fostering cross-disciplinary
+        research and facilitating academic exchange. As a respected publication, it provides a platform for scholars,
+        researchers, and professionals from various fields to share their findings, insights, and innovations. This
+        journal is published by Zas Digital Institute Training and Development Services (DITADS). The journal focuses on
+        the dissemination of high-quality research spanning a broad array of disciplines, particularly in environmental
+        studies, urban planning, sustainability, public administration, and other related fields.</p>
+      <p>
+        The journal serves as a vital resource for advancing knowledge and facilitating dialogue among researchers,
+        practitioners, and policymakers. By embracing interdisciplinary research, the Environmental and Urban Planning
+        International Journal (EUPIJ) aims to address the complex issues of today’s global society. It seeks to provide
+        well-rounded solutions and perspectives by integrating ideas from various sectors of knowledge, enabling more
+        holistic approaches to solving contemporary environmental and urban challenges.
+      </p>
+      <p>
+        Through its rigorous peer-review process and commitment to academic excellence, the Environmental and Urban
+        Planning International Journal (EUPIJ) aims to contribute significantly to the global research community. The
+        journal offers valuable insights into contemporary challenges and solutions, pushing the boundaries of academic
+        inquiry in its focus areas. By providing a forum for the exchange of diverse ideas and research, it continues to
+        promote innovation, foster intellectual growth, and inspire new thinking across disciplines.
+      </p>
+    </div>
+    <div>
+      <h1>Zas Digital Institute Training and Development Services (DITADS) is a duly registered entity under Philippine
+        law, demonstrating its commitment to transparency and compliance with the necessary legal frameworks. The
+        organization operates with the following official credentials:</h1>
+      <p><span class="font-semibold">• Business Permit No.:</span> BP-33229</p>
+      <p><span class="font-semibold">• Certificate No.:</span> 2019-25829</p>
+      <p><span class="font-semibold">• DTI Certificate of Business Registration No.:</span> 1033992</p>
+      <p><span class="font-semibold">• Philgeps No:</span> 201910-17877-1307663177</p>
+      <p><span class="font-semibold">• NBDB No.:</span> 7515</p>
+    </div>
+    <div class="space-y-2">
+      <p>DITADS is located at R188 Metro Square, Zone 2 Iponan, Cagayan de Oro City. These registrations underscore the
+        legitimacy and compliance of the journal with Philippine business regulations, ensuring full transparency,
+        accountability, and adherence to legal frameworks.</p>
+      <p>
+        With its well-established credentials and adherence to national and international standards, The Katatau Research
+        and Development Journal stands as a
+        cornerstone for academic excellence, bridging diverse fields of study and facilitating a global exchange of
+        knowledge and ideas.
+      </p>
+    </div>
+  </div>
+@endsection
